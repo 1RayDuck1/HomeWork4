c938853a7a7f5b30c22df53ae34ac3680d1238ac
Duck <kondratev_28@inbox.ru>
2022-12-13 00:23:37 +0300

    Taks 2 add

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -22,3 +22,25 @@ int ans = MathPow(number1, number2);
 Console.WriteLine($"Степень числа {number1} ровна {ans}.");
 */
 
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*
+int SumNumber(int num)
+{
+    int sum = 0;
+    while(num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ans = SumNumber(number);
+
+Console.WriteLine($"Сумма числа {number} ровна {ans}.");
+*/
+
