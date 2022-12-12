785b5d1233ab97f2466bcec8e87bc8366fa385e1
Duck <kondratev_28@inbox.ru>
2022-12-13 00:14:11 +0300

    Task 1 add

---
+++ HomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+/*
+int MathPow(int num1, int num2)
+{
+    int  number = num1;
+    while(num2 > 1)
+    {
+        num1 = num1 * number;
+        num2--;
+    }
+    return num1;
+}
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень числа: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int ans = MathPow(number1, number2);
+
+Console.WriteLine($"Степень числа {number1} ровна {ans}.");
+*/
+
